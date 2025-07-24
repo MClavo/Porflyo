47a17fa1110fad68cfdfe269eac35ba386e54e78
MClavo <m.clavo.conesa@gmail.com>
2025-07-24 09:39:32 +0200

    test: add JwtAdapterTest for comprehensive and fix directory name for JwtAdapterTest

--- core/src/test/java/com/porflyo/infrastructure/adapters/output/security/JwtAdapterTest.java
+++ core/src/test/java/com/porflyo/infrastructure/adapters/output/security/JwtAdapterTest.java
@@ -1,4 +1,4 @@
-package com.porflyo.infraestructure.adapters.output.security;
+package com.porflyo.infrastructure.adapters.output.security;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
