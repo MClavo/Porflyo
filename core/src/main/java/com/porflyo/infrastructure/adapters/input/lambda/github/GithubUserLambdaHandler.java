dcc3fe160334eeafe60e3a0839b5b9d8276d91e1
MClavo <m.clavo.conesa@gmail.com>
2025-07-24 20:11:03 +0200

    feat: implement GithubUserLambdaHandler for handling user requests via API Gateway

---
+++ core/src/main/java/com/porflyo/infrastructure/adapters/input/lambda/github/GithubUserLambdaHandler.java
@@ -0,0 +1,39 @@
+package com.porflyo.infrastructure.adapters.input.lambda.github;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.porflyo.application.ports.input.UserUseCase;
+import com.porflyo.application.ports.output.JwtPort;
+import com.porflyo.domain.model.GithubLoginClaims;
+import com.porflyo.domain.model.GithubUser;
+import com.porflyo.infrastructure.adapters.input.lambda.utils.LambdaHttpUtils;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class GithubUserLambdaHandler {
+    private final UserUseCase userService;
+    private final JwtPort jwtService;
+
+    @Inject
+    public GithubUserLambdaHandler(UserUseCase userService, JwtPort jwtService) {
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+
+    public APIGatewayV2HTTPResponse handleUserRequest(APIGatewayV2HTTPEvent input) {
+        try {
+            // Extract cookie and claims
+            String cookie = LambdaHttpUtils.extractCookieValue(input, "session");
+            GithubLoginClaims claims = jwtService.extractClaims(cookie);
+
+            // Get user data
+            GithubUser user = userService.getUserData(claims.getAccessToken());
+            return LambdaHttpUtils.createResponse(200, user.toString());
+
+        } catch (Exception e) {
+            return LambdaHttpUtils.createErrorResponse(500, e.getMessage());
+        }
+    }
+}
