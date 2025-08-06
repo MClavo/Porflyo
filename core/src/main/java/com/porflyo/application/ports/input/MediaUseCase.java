d3f3dbbd2e6f0e954ec02b87660beff0d2b99b2a
MClavo <m.clavo.conesa@gmail.com>
2025-08-06 18:27:54 +0200

    feat: Add MediaUseCase interface for media operations and PresignedPostDto for presigned POST requests

---
+++ core/src/main/java/com/porflyo/application/ports/input/MediaUseCase.java
@@ -0,0 +1,27 @@
+package com.porflyo.application.ports.input;
+
+import com.porflyo.domain.model.dto.PresignedPostDto;
+
+/**
+ * Use case interface for media operations, such as generating presigned URLs for file uploads
+ * and deleting objects from storage.
+ */
+public interface MediaUseCase {
+
+    /**
+     * Generates a presigned POST request for uploading files to a specified bucket and key.
+     *
+     * @param bucket The name of the storage bucket.
+     * @param key    The key (path) where the file will be uploaded.
+     * @return A PresignedPostDto containing the URL and fields for the presigned POST request.
+     */
+    PresignedPostDto generatePresignedPost(String bucket, String key);
+
+    /**
+     * Deletes an object from the specified storage bucket using its key.
+     *
+     * @param bucket The name of the storage bucket.
+     * @param key    The key (path) of the object to be deleted.
+     */
+    void delete(String bucket, String key);
+}
