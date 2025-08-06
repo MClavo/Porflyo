0d902cfb0d55afe5ecba23e73147b655112b275a
MClavo <m.clavo.conesa@gmail.com>
2025-08-06 19:35:48 +0200

    feat: Add PresignedPostGenerator for generating presigned POST requests to S3

--- core/src/main/java/com/porflyo/infrastructure/configuration/S3Config.java
+++ core/src/main/java/com/porflyo/infrastructure/configuration/S3Config.java
@@ -6,6 +6,9 @@ import io.micronaut.context.annotation.Requires;
 @ConfigurationProperties("s3")
 @Requires(property = "s3.endpoint")
 public record S3Config(
+    String accessKey,
+    String secretKey,
+    long expiration, // in minutes
     String region,
     String bucketName,
     String endpoint
