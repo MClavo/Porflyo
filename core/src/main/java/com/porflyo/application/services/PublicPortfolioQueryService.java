0203a117e38d07bb16a28446647ee2b44a87f811
MClavo <m.clavo.conesa@gmail.com>
2025-08-15 16:00:43 +0200

    feat: Implement PublicPortfolioQueryService for fetching published portfolios by URL with enhanced logging

---
+++ core/src/main/java/com/porflyo/application/services/PublicPortfolioQueryService.java
@@ -0,0 +1,72 @@
+package com.porflyo.application.services;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.porflyo.application.dto.PublicPortfolioView;
+import com.porflyo.application.ports.input.PublicPortfolioQueryUseCase;
+import com.porflyo.application.ports.output.PortfolioRepository;
+import com.porflyo.application.ports.output.PortfolioUrlRepository;
+import com.porflyo.domain.model.portfolio.Portfolio;
+import com.porflyo.domain.model.portfolio.PortfolioUrl;
+import com.porflyo.domain.model.portfolio.Slug;
+
+import jakarta.inject.Inject;
+
+public class PublicPortfolioQueryService implements PublicPortfolioQueryUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(PublicPortfolioQueryService.class);
+
+    private final PortfolioUrlRepository urlRepository;
+    private final PortfolioRepository portfolioRepository;
+
+    @Inject
+    public PublicPortfolioQueryService(PortfolioUrlRepository urlRepository, PortfolioRepository portfolioRepository) {
+        this.urlRepository = urlRepository;
+        this.portfolioRepository = portfolioRepository;
+    }
+
+    @Override
+    public Optional<PublicPortfolioView> getPublishedByUrl(Slug slugUrl) {
+        Optional<PortfolioUrl> fetchedUrl = urlRepository.findBySlug(slugUrl);
+        PortfolioUrl portfolioUrl = fetchedUrl.orElse(null);
+
+        // The portfolio can be a draft, portfolio has to be public
+        if(portfolioUrl == null || portfolioUrl.isPublic() == false) 
+            return Optional.empty();
+
+        Optional<Portfolio> portfolio = portfolioRepository.findById(
+                portfolioUrl.userId(),
+                portfolioUrl.portfolioId());
+
+        Portfolio fetchedPortfolio = portfolio.orElse(null);
+
+        // This should never happen. If it does fix ASAP.
+        if(fetchedPortfolio == null || fetchedPortfolio.isPublished() == false) {
+            if (fetchedPortfolio != null)
+                log.warn("Portfolio: {}, is not consistent with slug: {}", fetchedPortfolio, slugUrl);
+            else
+                log.warn("No portfolio found for slug: {}", slugUrl);
+
+            return Optional.empty();
+        }
+
+        return Optional.of(toPublicPortfolioView(fetchedPortfolio));
+
+    }
+
+    private PublicPortfolioView toPublicPortfolioView(Portfolio portfolio) {
+    return new PublicPortfolioView(
+            portfolio.id().value(),
+            portfolio.template(),
+            portfolio.title(),
+            portfolio.description(),
+            portfolio.sections()
+    );
+}
+
+
+}
+
