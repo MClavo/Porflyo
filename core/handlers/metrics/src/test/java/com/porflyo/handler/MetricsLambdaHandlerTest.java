7688ff767b3bdc4358086ba91cff2cb732a44618
MClavo <m.clavo.conesa@gmail.com>
2025-10-23 12:10:53 +0200

    refactor(MetricsLambdaHandlerTest): remove unused authUseCase mock from setup

--- core/handlers/metrics/src/test/java/com/porflyo/handler/MetricsLambdaHandlerTest.java
+++ core/handlers/metrics/src/test/java/com/porflyo/handler/MetricsLambdaHandlerTest.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
@@ -48,8 +46,7 @@ class MetricsLambdaHandlerTest {
 
     @BeforeEach
     void setUp() {
-        handler = new MetricsLambdaHandler(jsonMapper, bootstrapMapper, todayMapper, monthMapper, metricsUseCase,
-                authUseCase);
+        handler = new MetricsLambdaHandler(jsonMapper, bootstrapMapper, todayMapper, monthMapper, metricsUseCase);
 
         setupEventMocks();
         setupAuthMocks();
